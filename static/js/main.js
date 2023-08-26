41950aa7fc32eccfd415c858c6b2380716517486
fasiddiquee <fahad.siddiquee@momentum.co.za>
2023-08-26 15:12:05 +0530

    commit

--- static/js/main.js
+++ static/js/main.js
@@ -275,32 +275,6 @@
 
 })()
 
-
-function myFunction() {
-  var input, filter, table, tr, td, i;
-  input = document.getElementById("search");
-  filter = input.value.toUpperCase();
-  table = document.getElementById("socTable");
-  tr = table.getElementsByTagName("tr");
-  for (i = 0; i < tr.length; i++) {
-    let rowTds = tr[i].getElementsByTagName("td")
-    for (j = 0; j < rowTds.length; j++){
-      td = tr[i].getElementsByTagName("td")[j];
-      if (td) {
-        if (td.innerHTML.toUpperCase().indexOf(filter) > -1) {
-          tr[i].style.display = "";
-          break;
-        } else {
-          tr[i].style.display = "none";
-        }
-      }
-    }       
-  }
-}
-
-
-
-var table = $('#socTable').DataTable({
-  pageLength : 5,
-  lengthMenu: [[5, 10, 20, -1], [5, 10, 20, 'Todos']]
-})
+$("#toggleID").on("click", function() {
+  $(this).toggleClass('orange');
+});
